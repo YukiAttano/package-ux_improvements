b8fcd8869d310620d8dda5a28dcaaa96b60fd3e5
YukiAttano <2906.alexander@gmail.com>
2023-04-30 11:44:30 +0200

    added exports

--- lib/ux_improvements.dart
+++ lib/ux_improvements.dart
@@ -2,3 +2,6 @@ library ux_improvements;
 
 export 'src/fakeloading_widget.dart';
 export 'src/smooth_future_builder.dart';
+export 'src/shimmer/shimmer.dart';
+export 'src/shimmer/shimmer_box.dart';
+export 'src/shimmer/styles/shimmer_style.dart';
