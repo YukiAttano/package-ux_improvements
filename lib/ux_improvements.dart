94b0be5e9220523d2bbd291b07f14cdf22631ea5
YukiAttano <2906.alexander@gmail.com>
2023-09-19 18:22:51 +0200

    added export

--- lib/ux_improvements.dart
+++ lib/ux_improvements.dart
@@ -5,3 +5,4 @@ export 'src/smooth_future_builder.dart';
 export 'src/shimmer/shimmer.dart';
 export 'src/shimmer/shimmer_box.dart';
 export 'src/shimmer/styles/shimmer_style.dart';
+export 'src/implicit_animated_icon.dart';
