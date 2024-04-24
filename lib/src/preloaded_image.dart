6371ea1ce294bd9495e3faed6a6bb7f071a6e0d7
YukiAttano <2906.alexander@gmail.com>
2024-04-24 18:11:13 +0200

    added builder to preloaded image

--- lib/src/preloaded_image.dart
+++ lib/src/preloaded_image.dart
@@ -3,6 +3,13 @@ import 'package:flutter/material.dart';
 /// Preloads the image file to size the layout according to the image and available space.
 /// This allows an ink animation to be exactly on the image and not spreading over it.
 class PreloadedImage extends StatefulWidget {
+
+  /// builder used to allow implementations of animations
+  ///
+  /// if [image] is null, the image is not loaded yet.
+  /// if [image] is not null, it represents the full widget to display the image with all settings.
+  final Widget Function(Widget? image) builder;
+
   /// [DecorationImage.image] is the resolved image.
   /// after the resolving, [image] is placed in [decoration].
   /// The separation of the image is done for convenience.
@@ -16,25 +23,49 @@ class PreloadedImage extends StatefulWidget {
 
   final BoxFit boxFit;
 
-  final Widget loading;
-
   final void Function()? onPressed;
 
-  const PreloadedImage({
+  /// allows the full control of the image widget
+  /// all parameter are only effective on the loaded image.
+  const PreloadedImage.builder({
     super.key,
+    required this.builder,
     required this.image,
     ImageConfiguration? configuration,
     this.borderRadius,
     BoxDecoration? decoration,
     BoxFit? boxFit,
-    Widget? loading,
     this.onPressed,
   })  : configuration = configuration ?? ImageConfiguration.empty,
         decoration = decoration ?? const BoxDecoration(),
-        boxFit = boxFit ?? BoxFit.contain,
-        loading = loading ?? _loading;
+        boxFit = boxFit ?? BoxFit.contain;
+
+  /// replaces the image with a [loading] widget until it is fully loaded
+  PreloadedImage({
+    Key? key,
+    required DecorationImage image,
+    ImageConfiguration? configuration,
+    BorderRadius? borderRadius,
+    BoxDecoration? decoration,
+    BoxFit? boxFit,
+    Widget? loading,
+    void Function()? onPressed,
+  }) : this.builder(
+          key: key,
+          image: image,
+          configuration: configuration,
+          builder: (image) => _loadingBuilder(image, loading ?? _loading),
+          borderRadius: borderRadius,
+          decoration: decoration,
+          boxFit: boxFit,
+          onPressed: onPressed,
+        );
 
   static const _loading = Center(child: CircularProgressIndicator());
+
+  static Widget _loadingBuilder(Widget? image, Widget loading) {
+    return image ?? loading;
+  }
 
   @override
   State<PreloadedImage> createState() => _PreloadedImageState();
@@ -45,53 +76,65 @@ class _PreloadedImageState extends State<PreloadedImage> {
 
   Size? _size;
 
+  late Widget _child;
+
   @override
   void initState() {
     super.initState();
+
+    _buildChild();
     _requestImage();
   }
 
   void _requestImage() {
-    final stream = widget.image.image.resolve(widget.configuration);
+    ImageStream stream = widget.image.image.resolve(widget.configuration);
 
     stream.addListener(
       ImageStreamListener((image, synchronousCall) {
-        setState(() => _size = Size(image.image.width.toDouble(), image.image.height.toDouble()));
+        setState(() {
+          _size = Size(image.image.width.toDouble(), image.image.height.toDouble());
+          _buildChild();
+        });
       }),
     );
 
     _stream = stream;
   }
 
+  void _buildChild() {
+    _child = widget.builder(_size == null ? null : _loadedImageChild);
+  }
+
   @override
   Widget build(BuildContext context) {
-    if (_size != null) {
-      return LayoutBuilder(
-        builder: (context, constraints) {
-          final sizes = applyBoxFit(widget.boxFit, _size!, constraints.biggest);
-
-          return InkWell(
-            borderRadius: widget.borderRadius,
-            onTap: widget.onPressed,
-            child: Ink(
-              width: sizes.destination.width,
-              height: sizes.destination.height,
-              decoration: BoxDecoration(
-                color: widget.decoration.color,
-                gradient: widget.decoration.gradient,
-                shape: widget.decoration.shape,
-                border: widget.decoration.border,
-                boxShadow: widget.decoration.boxShadow,
-                backgroundBlendMode: widget.decoration.backgroundBlendMode,
-                borderRadius: widget.borderRadius,
-                image: widget.image,
-              ),
-            ),
-          );
-        },
-      );
-    }
-
-    return widget.loading;
+    return _child;
   }
+
+  /// the loaded image as a widget.
+  ///
+  /// must only be accessed, if [_size] is not null.
+  late final Widget _loadedImageChild = LayoutBuilder(
+    builder: (context, constraints) {
+      final sizes = applyBoxFit(widget.boxFit, _size!, constraints.biggest);
+
+      return InkWell(
+        borderRadius: widget.borderRadius,
+        onTap: widget.onPressed,
+        child: Ink(
+          width: sizes.destination.width,
+          height: sizes.destination.height,
+          decoration: BoxDecoration(
+            color: widget.decoration.color,
+            gradient: widget.decoration.gradient,
+            shape: widget.decoration.shape,
+            border: widget.decoration.border,
+            boxShadow: widget.decoration.boxShadow,
+            backgroundBlendMode: widget.decoration.backgroundBlendMode,
+            borderRadius: widget.borderRadius,
+            image: widget.image,
+          ),
+        ),
+      );
+    },
+  );
 }
