b97d48707fd809cb0d3f01e7ec89e6b9190861c6
YukiAttano <2906.alexander@gmail.com>
2025-03-21 14:14:11 +0100

    make analyzer happy

--- lib/src/smooth_future_builder.dart
+++ lib/src/smooth_future_builder.dart
@@ -5,6 +5,6 @@ import "package:flutter/widgets.dart";
 class SmoothFutureBuilder<T> extends FutureBuilder<T> {
   SmoothFutureBuilder({super.key, super.initialData, required super.builder, required Future<T> future, Duration delay = const Duration(milliseconds: 300)})
       : super(
-          future: future is SynchronousFuture ? future : Future.wait([future, Future.delayed(delay)], eagerError: true).then((value) => value.first),
+          future: future is SynchronousFuture ? future : Future.wait([future, Future<void>.delayed(delay)], eagerError: true).then((value) => value.first as T),
         );
 }
